72b42573338ecf138753bd03e5c5f47f7f37f23b
Oralita <oat431@gmail.com>
2021-07-09 08:06:52 +0700

    [🍖 update] : stock and quantity

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: true,
+            brand:'SE 331',
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity:50 ,onSale:true},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0 ,onSale:false}
             ],
+            selectedVariant:0,
             cart: 0
 
         }
@@ -20,6 +20,23 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index){
+            this.selectedVariant = index
+        }
+    },
+    computed : {
+        title(){
+            return this.brand + ' ' + this.product;
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSale(){
+            return this.variants[this.selectedVariant].onSale ? this.brand + ' ' + this.product + ' is on sale' : '' ;
         }
     }
 })
